22e2ef74dfe6e69e38d56c2055ba6b6c92691abf
Rakhmat <johndoe@example.com>
2025-04-02 08:45:51 +0500

    Добавлены настройки CORS для разрешения запросов с домена "https://abai.live" в файле main.ts и добавлен хост "abai.live" в конфигурацию предварительного просмотра в файле vite.config.ts.

--- server/main.ts
+++ server/main.ts
@@ -4,7 +4,11 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://abai.live",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 const router = express.Router();
 
