a4684d6938c7ec75efd6e9b460ae49cc8594648e
b1411 <johndoe@example.com>
2025-05-02 13:55:30 +0500

    ds

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-app.listen(8104, "::", () => {
-    console.log(`Server is running on port ${process.env.VITE_BACKEND_PORT} and hostname ${process.env.VITE_BACKEND_HOSTNAME}`);
+app.listen(8304, "::", () => {
+    console.log(`Server is running`);
 });
