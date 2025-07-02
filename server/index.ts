f84d461908a00fe6e674a173206b0f6bd9cb9202
b1411 <johndoe@example.com>
2025-07-02 14:11:26 +0500

    df

--- server/index.ts
+++ server/index.ts
@@ -4,5 +4,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 app.listen(8304, "::", () => {
-    console.log(`Server is running`);
+    console.log(`kolleddj server is running on port 8304`);
 });
