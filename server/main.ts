85386b7cf4cbdcf6049156407be70fd080d8cc6f
b1411 <johndoe@example.com>
2025-07-02 14:02:05 +0500

    feat: Обновлено разрешение CORS для поддержки всех источников и добавлен файл vite-env.d.ts

--- server/main.ts
+++ server/main.ts
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["https://abai.live", "https://backend.ab-ai.kz", "http://localhost:3000", "https://uib-ai.kz", "https://www.uib-ai.kz"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
