fdae520de9cd581542a18b60c6268c1fe2ea00d9
Бурабай Диас <eraliev.dias@gmail.com>
2025-07-16 09:00:13 +0500

    sdf

--- server/main.ts
+++ server/main.ts
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["https://*.abai.live", "https://backend.ab-ai.kz", "http://localhost:3000", "*.abai.live"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
